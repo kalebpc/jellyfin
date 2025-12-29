dc8a26bf5b579d89672878e436862d1a177a16f4
Kalebpc <113309697+kalebpc@users.noreply.github.com>
2025-12-29 16:23:07 -0500

    Refactor Rename-Folders.ps1 for improved parameters
    implement 'WhatIf' parameter

--- Rename-Folders.ps1
+++ Rename-Folders.ps1
@@ -10,23 +10,32 @@
 
 [CmdletBinding()]
 param(
-    [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, HelpMessage = "Path to folders.")]
-    [String[]]$Folders,
-    [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, HelpMessage = "List with updated folder names.")]
-    [String[]]$NewNames
+    [Parameter(Mandatory = $true, Position = 0, ValueFromPipelineByPropertyName = $true, HelpMessage = "List to folders.")]
+    [System.IO.DirectoryInfo[]]$Folders,
+    [Parameter(Position = 1, ValueFromPipelineByPropertyName = $true, HelpMessage = "List with updated folder names.")]
+    [String[]]$NewNames,
+    [Switch]$WhatIF
 )
+
 [Int64]$folderCount = $Folders.Count
+
 [Int64]$count = 0
+
 ForEach ( $folder In $Folders ) {
-    $title = $($(Split-Path -Path $folder -Leaf) -replace "\(.*$","").Trim()
-    "$folder"
+    $title = $($folder.Name -replace "\(.*$","").Trim()
+    # "{0}" -f $folder.FullName
     ForEach ( $name In $NewNames ) {
         If ( $name -ilike "*$title*" ) {
-            If ( ! $(Test-Path -LiteralPath "$(Split-Path -Path $folder -Parent)\$name" ) ) {
-                Rename-Item -LiteralPath "$folder" -NewName "$name"
+            If ( ! $(Test-Path -LiteralPath "$(Split-Path -Path $folder.FullName -Parent)\$name" ) ) {
+                If ($WhatIF) {
+                    "Rename-Item -LiteralPath '{0}' -NewName '{1}'" -f $folder.FullName, $name
+                } Else {
+                    Rename-Item -LiteralPath "$($folder.FullName)" -NewName "$name"
+                }
                 $count+=1
             }
         }
     }
 }
+
 "`nTotal folders     : '{0}'`nProcessed folders : '{1}'" -f $folderCount, $count
