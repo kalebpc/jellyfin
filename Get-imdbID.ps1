e7d52d309d1088e5dbd7f2beb2ff3e7be6515830
Kalebpc <113309697+kalebpc@users.noreply.github.com>
2025-12-29 16:19:15 -0500

    Implement rate limiting and WhatIf parameter
    Added rate limiting feature and WhatIf switch for testing.

--- Get-imdbID.ps1
+++ Get-imdbID.ps1
@@ -7,11 +7,6 @@
         ./Get-imdbID.ps1 -Apikey "YourOMDbApiKey" -Source "Movie Name (2005)", "Another Movie Name (2010)"
 
     Example Pipeline input :
-        # Handle Apikey from file.
-        $fileContent = Get-Content -Path ".env" | Where { $_ -imatch "omdbapikey" }
-        $keyapi = $($fileContent -split "=")[1].Replace("'","")
-        $keyapi = $keyapi.Replace('"','').Trim()
-
         $temp = [PSCustomObject]@{
             'Apikey' = $keyapi
             'Source' = @(# List of Movie names.)
@@ -34,10 +29,13 @@
 
 [CmdletBinding()]
 param(
-    [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true HelpMessage = "OMDbAPI key or Path to file with key. Example file contents: 'omdbapikey' = 'yourOMDbAPIkey' ")]
+    [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, HelpMessage = "OMDbAPI key or Path to file with key. Example file contents: 'omdbapikey' = 'yourOMDbAPIkey' ")]
     [String]$Apikey,
     [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, HelpMessage = "List of folder/Movie names.")]
-    [String[]]$Source
+    [String[]]$Source,
+    [Parameter(HelpMessage = "Seconds between api requests.", ValueFromPipelineByPropertyName = $true)]
+    [Int32]$RateLimit = 5,
+    [Switch]$WhatIF
 )
 
 $Apikey = $Apikey.Trim()
@@ -46,7 +44,6 @@ $results = New-Object PSObject
 $results | Add-Member -MemberType NoteProperty -Name "Processed" -Value (New-Object System.Collections.Generic.List[String])   
 $results | Add-Member -MemberType NoteProperty -Name "Skipped" -Value (New-Object System.Collections.Generic.List[String])   
 $results | Add-Member -MemberType NoteProperty -Name "Errors" -Value (New-Object System.Collections.Generic.List[String])   
-$results | Add-Member -MemberType NoteProperty -Name "Debug" -Value (New-Object System.Collections.Generic.List[String])   
 
 # Required for [System.Web.HttpUtility]::UrlEncode()
 If ( -not ('System.Web.HttpUtility' -as [type]) ) { Add-Type -AssemblyName System.Web }
@@ -68,27 +65,31 @@ If ( $Apikey -ine "" -and $Source -ine "" ) {
         If ( $year -ieq "" ) { Add-Content -LiteralPath $Output -Value "$title - Skipped. No Year found." ; return }
         
         $url = "http://www.omdbapi.com/?apikey={0}&t={1}&y={2}" -f $Apikey, [System.Web.HttpUtility]::UrlEncode($title), $year
-        $results.Debug.Add($url)
-        
-        # Rate limit requests.
-        Start-Sleep -Seconds 5
 
-        Try {
-            $response = Invoke-RestMethod -Uri $url
-        } Catch {
-            $results.Errors.Add($_)
-        }
+        If (!$WhatIF) {
+            # Rate limit requests.
+            Start-Sleep -Seconds $RateLimit
 
-        [String]$id = $response.imdbID
+            Try {
+                $response = Invoke-RestMethod -Uri $url
+            } Catch {
+                $results.Errors.Add($_)
+            }
 
-        If ( $id -imatch "^[tT][tT]" -and $id -ine "" ) {
+            [String]$id = $response.imdbID
 
-            $newname = "{0} [imdbid-{1}]" -f $Name, $id
+            If ( $id -imatch "^[tT][tT]" -and $id -ine "" ) {
 
-            $results.Processed.Add($newname)
+                $newname = "{0} [imdbid-{1}]" -f $Name, $id
+
+                $results.Processed.Add($newname)
+
+            } Else {
+                $results.Skipped.Add($Name)
+            }
 
         } Else {
-            $results.Skipped.Add($Name)
+            $results.Processed.Add($url)
         }
     }
     return $results
